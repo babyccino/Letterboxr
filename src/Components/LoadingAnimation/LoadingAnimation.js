700e5af94be84e5a05d869a80ceba2d38f86015a
Gus Ryan <gus.ryan163@gmail.com>
2022-05-24 22:32:09 -0700

    refactored loading animation to component

---
+++ src/Components/LoadingAnimation/LoadingAnimation.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './LoadingAnimation.scss';
+
+const LoadingAnimation = () => (<div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>);
+
+export default LoadingAnimation;
